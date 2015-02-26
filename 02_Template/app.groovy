874e108f7bcf4b1a8c218bcf45281bd3083cdc7b
Masaki Tatematsu <neogym@users.noreply.github.com>
2015-02-26 14:21:26 +0900

    Add 02_Template

---
+++ 02_Template/app.groovy
@@ -0,0 +1,14 @@
+@Grab("thymeleaf-spring4")
+
+@Controller
+class MyBootApp {
+
+    @RequestMapping("/")
+    @ResponseBody
+    def top(ModelAndView mv) {
+        mv.setViewName("index")
+        mv.addObject("title", "Hello!")
+        mv.addObject("msg", "welcome to Thymeleaf!")
+    }
+
+}
