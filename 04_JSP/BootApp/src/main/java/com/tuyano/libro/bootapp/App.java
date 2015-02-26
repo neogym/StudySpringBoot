01d934cd6b038ca816fa5220e0e8c58c53519271
Masaki Tatematsu <neogym@users.noreply.github.com>
2015-02-26 15:55:18 +0900

    Add 04_JSP

---
+++ 04_JSP/BootApp/src/main/java/com/tuyano/libro/bootapp/App.java
@@ -0,0 +1,19 @@
+package com.tuyano.libro.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+ 
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+    }
+
+}
+
